2ce13000fcf0a1fd21db4d9ddca20ce14bb5cee2
m.Shahrestanaki <m.shahrestanaki@shaparak.com>
2025-05-26 17:07:31 +0330

    start projects - read file 2

--- src/main/java/com/msh/service/ReadConfigSrv.java
+++ src/main/java/com/msh/service/ReadConfigSrv.java
@@ -8,7 +8,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author m.Shahrestanaki @createDate 5/26/2025
@@ -17,8 +21,15 @@ import java.util.Arrays;
 @Service
 public class ReadConfigSrv implements IReadConfigSrv{
 
+    private final SaveDataSrv saveDataSrv;
+
+    public ReadConfigSrv(SaveDataSrv saveDataSrv) {
+        this.saveDataSrv = saveDataSrv;
+    }
+
     @Override
     public void readFile(InputDto input) {
+        List<String> data = new ArrayList<>() ;
         try {
             String url = input.getUrl();
             HttpClient client = HttpClient.newHttpClient();
@@ -29,9 +40,17 @@ public class ReadConfigSrv implements IReadConfigSrv{
                 Arrays.stream(response.body().split("\n"))
                         .map(String::trim)
                         .filter(line -> line.startsWith("vless://") || line.startsWith("vlessl://"))
-                        .forEach(System.out::println);
+                        .forEach(data::add);
             } else {
                 System.out.println("Failed to fetch content. Status: " + response.statusCode());
+            }
+            if(!data.isEmpty()){
+                boolean result = saveDataSrv.saveToFile(data,"132456.txt");
+                if(result){
+                    Path path = Paths.get(System.getProperty("user.dir")).resolve("132456.txt");
+                    //String repo, String path, String branch, Path localFilePath, String commitMessage
+                    saveDataSrv.GithubUploader("shahrestanaki/v2config","132456.txt","master",path.getFileName(),"created");
+                }
             }
         }catch (Exception e){
             log.error("error : ",e);
